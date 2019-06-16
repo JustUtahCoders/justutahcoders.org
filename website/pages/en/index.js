b3207400cad381b012904d28268cbf6f2e9a6781
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2019-06-15 21:05:00 -0600

    some sizing adjustments and a hover toggle

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -10,7 +10,7 @@ class Index extends React.Component {
           <h2>Every industry needs software developers. Non-profits and community organizations are no exception.</h2>
           <div className="purpose-statement-image-container">
             <img src="/img/mobile-application-lean.svg" alt="stylized man leaning on giant mobile phone"/>
-            <h3 id="phone-message">What can we make for you?</h3>
+            <p id="phone-message">What can we make for you?</p>
           </div>
         </section>
       </div>
