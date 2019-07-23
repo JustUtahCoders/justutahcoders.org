ff843a3e8dadf4208d6ce0b646d0798857317762
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2019-07-23 14:06:02 -0600

    update

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -6,7 +6,6 @@ export default function ContactUs() {
     return (
         <main className="contact-container app-container">
             <p>Interested in starting a project with us? Reach out at <a href="mailto:justutahcoders@gmail.com">justutahcoders@gmail.com</a></p>
-            {/* need phone number for here */}
         </main>
     )
 }
