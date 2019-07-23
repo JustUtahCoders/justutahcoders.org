927f516c7a5ca55fc7bc0e32466f146539990878
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2019-07-23 15:00:29 -0600

    added page boilerplate

---
+++ client/src/pages/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../static/style/blog.css'
+
+export default function Blog () {
+    return (
+        <main className="blog-container page-container">
+            This is the blog
+        </main>
+    )
+}
